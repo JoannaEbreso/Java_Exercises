1a51c1c6a89c39e6155a5329ec4e84c0c102c906
unknown <jebreso@yahoo.com>
2021-02-18 05:04:11 +0100

    average value with lambda

--- src/AverageValue.java
+++ src/AverageValue.java
@@ -1,2 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class AverageValue {
+    public double findAverageValue(List<Integer> listOfNumbers) {
+
+        return listOfNumbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .getAsDouble();
+    }
 }
--- test/AverageValueTest.java
+++ test/AverageValueTest.java
@@ -1,10 +1,24 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AverageValueTest {
 
+    AverageValue averageValueObject;
     @BeforeEach
     void setUp() {
+        averageValueObject = new AverageValue();
+    }
+
+    @Test
+    void averageValueOfAListOfValues_CanBeFound(){
+        List<Integer> listOfNumbers = Arrays.asList(10,20,30,40,50);
+
+        assertEquals(30,averageValueObject.findAverageValue(listOfNumbers));
     }
 }
