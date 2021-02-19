827c9dc2534cd202cd7f8efde5801e6f7809c888
unknown <jebreso@yahoo.com>
2021-02-19 17:53:51 +0100

    filtered strings exercise with lambdas

--- src/FilterString.java
+++ src/FilterString.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class FilterString {
 
+    public List<String> filterList(List<String> listOfStrings) {
+
+        Predicate<String> containsLetterAAndThreeCharacters =
+                word -> (word.startsWith("a") && word.length() == 3);
+
+        return listOfStrings.stream()
+                .filter(containsLetterAAndThreeCharacters)
+                .collect(Collectors.toList());
+    }
 }
--- test/FilterStringTest.java
+++ test/FilterStringTest.java
@@ -1,10 +1,24 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilterStringTest {
 
+    FilterString filterStringObject;
     @BeforeEach
     void setUp() {
+        filterStringObject = new FilterString();
+    }
 
+    @Test
+    void testThatWeCanFilterAListOfStrings(){
+        List<String> listOfStrings = Arrays.asList("and","guy","any","app");
+        List<String> filteredList = filterStringObject.filterList(listOfStrings);
+        assertEquals(3,filteredList.size());
+    }
 }
